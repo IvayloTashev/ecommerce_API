145cd51c0ab41116f3e8dcee45efa4268c3a6adc
IvayloTashev <145584765+IvayloTashev@users.noreply.github.com>
2024-12-23 12:22:16 +0200

    Add models and initialize the routes

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const userRoute = require("./routes/user")
+
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 
-mongoose.connect(
-    "mongodb+srv://Admin:admin@cluster0.0bhma.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0"
-    ).then(() => console.log("Database connected successfully!")).catch((err) => console.log(err));
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Database connected successfully!"))
+    .catch((err) => console.log(err)
+);
+
+app.use(express.json());
+app.use("/api/user", userRoute);
+
 
 app.listen(5000, () => {
     console.log("Server is listening on port 5000...");
